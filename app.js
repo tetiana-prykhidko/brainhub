9e06e9835a39f87afa0ac3618a7ef274ee7a6287
Tetiana <tetiana_prykhidko@ukr.net>
2020-04-02 21:27:52 +0300

    add fixed-header

---
+++ app.js
@@ -0,0 +1,24 @@
+$(function() {
+
+	let header = $("#header");
+	let intro = $("#intro");
+	let introH;
+	let scrollPos = $(window).scrollTop();
+
+	$(window).on("scroll load resize", function() {
+		introH = intro.innerHeight();
+		scrollPos = $(this).scrollTop();
+
+		console.log(introH)
+
+		if(scrollPos > introH) {
+			header.addClass("fixed");
+		} else {
+			header.removeClass("fixed");
+		}
+	});
+
+	
+
+
+	});
