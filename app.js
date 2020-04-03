b58f80e25e2ec0da70aea3111ef5d2985a7dd625
Tetiana <tetiana_prykhidko@ukr.net>
2020-04-03 14:52:56 +0300

    add smooth scroll

--- app.js
+++ app.js
@@ -1,24 +1,57 @@
 $(function() {
 
+	/* Fixed header */
 	let header = $("#header");
 	let intro = $("#intro");
-	let introH;
+	let introH = intro.innerHeight();
 	let scrollPos = $(window).scrollTop();
+	let nav = $("#nav");
+	let navToggle = $("#navToggle");
 
-	$(window).on("scroll load resize", function() {
+	checkScroll(scrollPos, introH);
+
+	$(window).on("scroll resize", function() {
 		introH = intro.innerHeight();
 		scrollPos = $(this).scrollTop();
 
-		console.log(introH)
+		checkScroll(scrollPos, introH);
 
-		if(scrollPos > introH) {
+	});
+
+	function checkScroll(scrollPos, introH) {
+		if( scrollPos > introH ) {
 			header.addClass("fixed");
 		} else {
 			header.removeClass("fixed");
 		}
+	}
+
+
+	/* Smooth scroll */
+	$("[data-scroll]").on("click", function(event) {
+		event.preventDefault();
+
+		let elementId = $(this).data('scroll');
+		let elementOffset = $(elementId).offset().top;
+
+		nav.removeClass("show");
+
+
+		$("html, body").animate({
+			scrollTop: elementOffset - 65
+		}, 700);
 	});
 
-	
+	/* NavToggle */
+	let nav = $("#nav");
+	let navToggle = $("#navToggle");
 
+	navToggle.on("click", function(event) {
+		event.preventDefault();
+
+		nav.toggleClass("show");
 
 	});
+
+
+});
